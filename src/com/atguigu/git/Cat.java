62c52f8ca9b4b646ebe20011ea5790ad0b99e13a
zzyybs <zzyybs@126.com>
2015-08-24 17:31:59 +0800

    eat fish

--- src/com/atguigu/git/Cat.java
+++ src/com/atguigu/git/Cat.java
@@ -4,6 +4,7 @@ public class Cat
 {
 	public static void main(String[] args)
 	{
-		System.out.println("github update cat......");	
+		System.out.println("github update cat......");
+		System.out.println("cat is eatting fish....");
 	}
 }
