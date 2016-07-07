87c1247946568f56525a4242183a252a91b435f1
zzyybs <zzyybs@126.com>
2016-07-07 18:18:56 +0800

    Update Cat.java

--- src/com/atguigu/git/Cat.java
+++ src/com/atguigu/git/Cat.java
@@ -5,8 +5,5 @@ public class Cat
 	public static void main(String[] args)
 	{
 		System.out.println("github update cat......");
-		System.out.println("cat is eatting fish....");
-		System.out.println("Client getInfo......2015.8.25");
-		System.out.println("Github send,好消息");
 	}
 }
